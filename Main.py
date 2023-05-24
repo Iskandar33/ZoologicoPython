cafe014128a5a1f439357bf53e6c16bbb6bd5688
Iskandar33 <phanor05@outlook.es>
2023-05-23 23:42:10 -0500

    unu

--- Main.py
+++ Main.py
@@ -8,3 +8,5 @@ if __name__ == "__main__":
     controlador = controller.sistemaController(modelo, vista)
 
     
+
+    
--- controller/sistemaController.py
+++ controller/sistemaController.py
@@ -9,45 +9,45 @@ class ZoologicoController:
         self.modelo = modelo
         self.vista = vista
 
-    def añadirHabitat(self):
-        self.vista.imprimir("Hay 4 hábitats posibles para crear, estos son:")
-        self.vista.imprimir("1. Desértico \n2. Selvático \n3. Polar \n4. Acuático")
-        try:
-            opc = self.vista.obtenerHabitat(self.modelo.habitats)
-            self.modelo.añadirHabitat(opc)
-        except IndexError as e:
-            return str(e)
+    # def añadirHabitat(self):
+    #     self.vista.imprimir("Hay 4 hábitats posibles para crear, estos son:")
+    #     self.vista.imprimir("1. Desértico \n2. Selvático \n3. Polar \n4. Acuático")
+    #     try:
+    #         opc = self.vista.obtenerHabitat(self.modelo.habitats)
+    #         self.modelo.añadirHabitat(opc)
+    #     except IndexError as e:
+    #         return str(e)
         
-    def añadirAnimalHabitat(self):
-        if(self.modelo._contAnimales == 0):
-            self.vista.imprimir("No hay animales en el zoologico actualmente")
-        else:
-            try: 
-                pTempAnimal = self.modelo.crearAnimal(self.modelo._contAnimales)
-                opc = self.vista.mostrarHabitats(self.modelo._arrayHabitats)
-                habitatEscogido = self.modelo._arrayHabitats[opc]
-                self.modelo.añadirAnimalHabitat(habitatEscogido, pTempAnimal)
-            except IndexError as e:
-                return str(e)
+    # def añadirAnimalHabitat(self):
+    #     if(self.modelo._contAnimales == 0):
+    #         self.vista.imprimir("No hay animales en el zoologico actualmente")
+    #     else:
+    #         try: 
+    #             pTempAnimal = self.modelo.crearAnimal(self.modelo._contAnimales)
+    #             opc = self.vista.mostrarHabitats(self.modelo._arrayHabitats)
+    #             habitatEscogido = self.modelo._arrayHabitats[opc]
+    #             self.modelo.añadirAnimalHabitat(habitatEscogido, pTempAnimal)
+    #         except IndexError as e:
+    #             return str(e)
 
-    def crearAnimal(self, contAnimales):
-        self.vista.imprimir("Cada hábitat tiene 6 especies de animales para agregar, escoja el hábitat que desea")
-        self.vista.imprimir("0. Desértico \n1. Selvático \n2. Polar \n3. Acuático")
-        try:
-            opcHabitat = self.vista.obtenerHabitatAnimal()
-            animales = self.modelo.animalesHabitat[opcHabitat]
-            opcAnimal = self.vista.mostrarAnimales(animales)
-            nombre = self.vista.obtenerNombre()
-            especie = opcAnimal
-            habitatPertenece = opcHabitat
-            horasDormir = self.vista.obtenerHorasDormir()
-            print("Dietas a escoger:\n0.Carnivoro\n1.Herbivoro\n2.Omnivoro")
-            opcDieta = self.vista.obtenerDieta()
-            pTempAnimal = self.modelo.crearAnimal(contAnimales, nombre, especie, habitatPertenece, opcDieta, horasDormir)
-            self.vista.imprimir("El animal fue creado.")
-        except IndexError as e:
-            return str(e)
-        return pTempAnimal
+    # def crearAnimal(self, contAnimales):
+    #     self.vista.imprimir("Cada hábitat tiene 6 especies de animales para agregar, escoja el hábitat que desea")
+    #     self.vista.imprimir("0. Desértico \n1. Selvático \n2. Polar \n3. Acuático")
+    #     try:
+    #         opcHabitat = self.vista.obtenerHabitatAnimal()
+    #         animales = self.modelo.animalesHabitat[opcHabitat]
+    #         opcAnimal = self.vista.mostrarAnimales(animales)
+    #         nombre = self.vista.obtenerNombre()
+    #         especie = opcAnimal
+    #         habitatPertenece = opcHabitat
+    #         horasDormir = self.vista.obtenerHorasDormir()
+    #         print("Dietas a escoger:\n0.Carnivoro\n1.Herbivoro\n2.Omnivoro")
+    #         opcDieta = self.vista.obtenerDieta()
+    #         pTempAnimal = self.modelo.crearAnimal(contAnimales, nombre, especie, habitatPertenece, opcDieta, horasDormir)
+    #         self.vista.imprimir("El animal fue creado.")
+    #     except IndexError as e:
+    #         return str(e)
+    #     return pTempAnimal
     
     def ingresarAccionAnimal(self):
         if(self.modelo._contAnimales == 0):
@@ -81,7 +81,13 @@ class ZoologicoController:
             col3.subheader("Ingresarle una acción a un animal")
             boton_accion_animal = col3.button("Acceder a esta opción", 3)
         
-        ZoologicoController.ejecutarOpcion()
+        if(boton_añadir_habitat ==  True):
+            st.session_state["opcion"] = 1
+        elif(boton_añadir_animal == True):
+            st.session_state["opcion"] = 2
+        elif(boton_accion_animal == True):
+            st.session_state["opcion"] = 3
+        
 
     def menuAñadirHabitat(self):
         st.divider()
@@ -96,7 +102,10 @@ class ZoologicoController:
         if(self.modelo._contAnimales == 0):
             self.vista.imprimir("No hay animales en el zoologico actualmente")
         else:
-            self.menuCrearAnimal()
+            pTempAnimal = self.menuCrearAnimal()
+            opc = self.vista.mostrarHabitats(self.modelo._arrayHabitats)
+            habitatEscogido = self.modelo._arrayHabitats[opc]
+            self.modelo.añadirAnimalHabitat(habitatEscogido, pTempAnimal)
 
     def menuCrearAnimal(self):
         st.divider()
@@ -111,6 +120,6 @@ class ZoologicoController:
             dieta = st.selectbox("Dietas a escoger:\n0.Carnivoro\n1.Herbivoro\n2.Omnivoro"
                                  (self.modelo.tipoDieta))
             pTempAnimal = Animal(self.modelo._contAnimales, nombre, animal, habitat, dieta, horasDormir)
-        
+        return pTempAnimal
             
         
